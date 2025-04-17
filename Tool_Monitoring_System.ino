9e056f982d81b911e1d1d85b2dfef76afb464555
Mowah Chibuike <mctimmykings@gmail.com>
2025-04-17 03:11:16 +0100

    Initialized the sketch

---
+++ Tool_Monitoring_System.ino
@@ -0,0 +1,17 @@
+// Inteprete signals from the Zero-Crossing Detection Circuit (ZCDC)
+// Handle toggling action for the pushbuttons
+// Take potentiometer readings if control is manual
+// Turn on and off the SCR based on the signal from the ZCDC and potentiometer readings
+// Monitor temperature from temperature sensor
+// Use IR sensor as a tachometer
+// Monitor voltage from voltage sensor
+// Monitor current from current sensor
+
+void setup() {
+
+}
+
+void loop() {
+
+
+}
